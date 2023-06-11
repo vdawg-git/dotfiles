5f3aa2e48039b5924d1b0261d06807c5ea7f609f
Christofer <imchris@tuta.io>
2023-06-11 12:41:32 +0200

    stuff

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,9 @@
+# VS Code shell integration - see https://code.visualstudio.com/docs/terminal/shell-integration
+string match -q "$TERM_PROGRAM" "vscode"
+and . (code --locate-shell-integration-path fish)
+
 export EDITOR="code --wait"
+# set EDITOR("code --wait")
 
 alias pn="pnpm"
 alias cz="chezmoi"
