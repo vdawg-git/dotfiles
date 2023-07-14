4b120272df9cebb54b49e4e5839dc8e8af373fa5
Christofer <imchris@tuta.io>
2023-07-14 15:53:15 +0200

    .

--- runonce_setup.sh
+++ runonce_setup.sh
@@ -1,2 +1,10 @@
 # Fishery - Fish plugin manager
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+# Allow Flatpak apps to be styled
+sudo flatpak override --filesystem=$HOME/.themes
+sudo flatpak override --filesystem=$HOME/.icons
+# And apply the theme settings
+sudo flatpak override --env=GTK_THEME=Colloid-Orange-Dark-Gruvbox
+sudo flatpak override --env=ICON_THEME=WhiteSur-orange-dark
+
