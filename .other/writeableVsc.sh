7642e0d9e4c673034c72e3bd454bf43c153640c6
VDawg <vdawg@tuta.io>
2024-02-18 15:04:53 +0100

    .

--- .other/writeableVsc.sh
+++ .other/writeableVsc.sh
@@ -5,7 +5,7 @@ echo "Making VS Code writeable.."
 # Allow writing to VS Code to enable custom css modifications
 sudo chown -R $(whoami) $(which code)
 sudo chown -R $(whoami) /opt/visual-studio-code
-sudo chown -R $(whoami) $(which code-oss)
-sudo chown -R $(whoami) /opt/code-translucent
+# sudo chown -R $(whoami) $(which code-oss)
+# sudo chown -R $(whoami) /opt/code-translucent
 
 echo "Made VS Code writeable.."
