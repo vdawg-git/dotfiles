e03682979c9f7d1700472d61e0127d45b1bbdbd1
VDawg <vdawg@tuta.io>
2024-05-27 22:18:49 +0300

    .

--- run_once_before_01_install_pkg.sh
+++ run_once_before_01_install_pkg.sh
@@ -6,60 +6,14 @@
 
 echo "Installing packages..";
 cd $(chezmoi source-path);
+echo $PWD;
 
 if [ $? -ne 0 ] ; then
     echo "Error: unable to source global_fn.sh, please execute from $(dirname $(realpath $0))..."
     exit 1
 fi
 
-install_list="${1:-'.install_pkg.lst'}"
-
-if ! pkg_installed git
-    then
-    echo "installing dependency git..."
-    sudo pacman -S git
-fi
-
-if ! pkg_installed yay
-    then
-    echo "installing dependency yay..."
-    ./install_yay.sh 2>&1
-fi
-
-
-while read pkg
-do
-    if pkg_installed ${pkg}
-        then
-        echo "skipping ${pkg}..."
-
-    elif pkg_available ${pkg}
-        then
-        echo "queueing ${pkg} from arch repo..."
-        pkg_arch=`echo $pkg_arch ${pkg}`
-
-    elif aur_available ${pkg}
-        then
-        echo "queueing ${pkg} from aur..."
-        pkg_aur=`echo $pkg_aur ${pkg}`
-
-    else
-        echo "error: unknown package ${pkg}..."
-    fi
-done < $install_list
-
-
-if [ `echo $pkg_arch | wc -w` -gt 0 ]
-then
-    echo "installing $pkg_arch from arch repo..."
-    sudo pacman -S $pkg_arch
-fi
-
-if [ `echo $pkg_aur | wc -w` -gt 0 ]
-then
-    echo "installing $pkg_aur from aur..."
-    yay -S $pkg_aur
-fi
+install_list="${1:-.install_pkg.lst}"
 
 pkg_installed()
 {
@@ -108,4 +62,46 @@ aur_available()
         return 1
     fi
 }
+
+if ! pkg_installed git
+    then
+    echo "installing dependency git..."
+    sudo pacman -S git
+fi
+
+
+while read pkg
+do
+    if pkg_installed ${pkg}
+        then
+        echo "skipping ${pkg}..."
+
+    elif pkg_available ${pkg}
+        then
+        echo "queueing ${pkg} from arch repo..."
+        pkg_arch=`echo $pkg_arch ${pkg}`
+
+    elif aur_available ${pkg}
+        then
+        echo "queueing ${pkg} from aur..."
+        pkg_aur=`echo $pkg_aur ${pkg}`
+
+    else
+        echo "error: unknown package ${pkg}..."
+    fi
+done < $install_list
+
+
+if [ `echo $pkg_arch | wc -w` -gt 0 ]
+then
+    echo "installing $pkg_arch from arch repo..."
+    sudo pacman -S $pkg_arch
+fi
+
+if [ `echo $pkg_aur | wc -w` -gt 0 ]
+then
+    echo "installing $pkg_aur from aur..."
+    yay -S $pkg_aur
+fi
+
 
