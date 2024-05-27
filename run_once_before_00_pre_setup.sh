91ab4e2933da451f9b5b93c8f74083647bf581b3
VDawg <vdawg@tuta.io>
2024-05-27 21:57:01 +0300

    .

--- run_once_before_00_pre_setup.sh
+++ run_once_before_00_pre_setup.sh
@@ -10,8 +10,8 @@ sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-sudo pacman -S tree --noconfirm
-echo -e "[chaotic-aur] \n Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tree -a /etc/pacman.conf
+echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 
 # Install Rust via rustup
 # We do this before installing other packages to prevent them using Rust from the Arch repo
