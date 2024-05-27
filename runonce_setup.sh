5c838cd7ba5066d26e40090514100a549068a8bc
VDawg <vdawg@tuta.io>
2024-05-27 17:34:48 +0300

    stuff

--- runonce_setup.sh
+++ runonce_setup.sh
@@ -1,12 +1,15 @@
+echo "Running dotfiles setup.."
+
 # Chaotic Aur
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
+sudo pacman -S tree
 echo -e "[chaotic-aur] \n Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tree -a /etc/pacman.conf
 
-# Install rus via rustup
+# Install Rust via rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Install packages
@@ -18,7 +21,6 @@ popd
 
 # Fishery - Fish plugin manager
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-
 
 # Set fish as the default shell
 chsh -s /bin/fish
