d6c0dc95767b28fffd1a505484136074e35e3bf0
VDawg <vdawg@tuta.io>
2024-05-28 10:07:23 -0400

    better chatoic aur setup

--- run_once_before_00_pre_setup.sh
+++ run_once_before_00_pre_setup.sh
@@ -10,9 +10,7 @@ sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
-
+grep chaotic-aur /etc/pacman.conf || sudo sh -c "echo -e '[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' >>/etc/pacman.conf" 
 sudo pacman -Sy
 
 # Install Rust via rustup
