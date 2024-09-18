f1c307eaa966946ada39ad2fbc1f75947577c7be
VDawg <vdawg@tuta.io>
2024-09-18 23:48:44 +0300

    nvim: more

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -27,7 +27,27 @@ return {
 		end,
 	},
 
+	{ 'echasnovski/mini.ai', version = '*'  },
+	{ 'echasnovski/mini.move', version = '*' },
+
+	--  -----------------------------------------------
 	--  Plugins for Neovim in the terminal
+	--  -----------------------------------------------
+	{ "tpope/vim-commentary", cond = not isVscode },
+	{ "luckasRanarison/tree-sitter-hypr", cond = not isVscode },
+	{ "NvChad/nvim-colorizer.lua", config = true, cond = not isVscode },
+	{ "nvim-tree/nvim-web-devicons", cond = not isVscode },
+
+	{ 'echasnovski/mini.visits', version = '*', cond = not isVscode },
+	{ 'echasnovski/mini.statusline', version = '*', cond = not isVscode },
+	{ 'echasnovski/mini.pairs', version = '*', cond = not isVscode },
+	{ 'echasnovski/mini.cursorword', version = '*', cond = not isVscode },
+	{ 'echasnovski/mini.comment', version = '*', cond = not isVscode },
+	{ 'echasnovski/mini.fuzzy', version = '*', cond = not isVscode },
+	{ 'echasnovski/mini-git', version = '*', cond = not isVscode },
+	{ 'echasnovski/mini.animate', version = '*', cond = not isVscode },
+
+
 	{
 		"nvim-tree/nvim-tree.lua",
 		cond = not isVscode,
@@ -47,26 +67,4 @@ return {
 			vim.cmd("autocmd Colorscheme * highlight NvimTreeNormal guibg=#00000000 guifg=#9da5b3")
 		end,
 	},
-	{ "nvim-tree/nvim-web-devicons", cond = not isVscode },
-	{ "tpope/vim-commentary", cond = not isVscode },
-	{ "luckasRanarison/tree-sitter-hypr", cond = not isVscode },
-	{ "NvChad/nvim-colorizer.lua", config = true, cond = not isVscode },
-	
-	-- { "lukas-reineke/indent-blankline.nvim", main = "ibl", config = function() 
-
-	-- 	local highlight = {
-    -- "CursorColumn",
-    -- "Whitespace",
--- }
-
--- require("ibl").setup {
-    -- indent = { highlight = highlight, char = "" },
-    -- whitespace = {
-        -- highlight = highlight,
-        -- remove_blankline_trail = false,
-    -- },
--- }
-
-	-- end, cond = not isVscode
--- }
 }
