36ee70ebfac5996e7fb403b2b6d5a361f428a16e
VDawg <vdawg@tuta.io>
2026-09-10 20:20:01 +0300

    fix swap

--- hosts/swordfish/host.nix
+++ hosts/swordfish/host.nix
@@ -1,7 +1,16 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [./hardware.nix];
 
   networking.hostName = "swordfish";
+
+  boot.initrd.luks.devices."luks-fa3f1ad7-d3d6-4abe-b3f5-e3f2781ffab3".device = "/dev/disk/by-uuid/fa3f1ad7-d3d6-4abe-b3f5-e3f2781ffab3";
+  swapDevices = lib.mkForce [
+    {device = "/dev/disk/by-uuid/795c1cf1-6107-41d6-8bc3-f064db351809";}
+  ];
 
   environment.systemPackages = with pkgs; [
   ];
